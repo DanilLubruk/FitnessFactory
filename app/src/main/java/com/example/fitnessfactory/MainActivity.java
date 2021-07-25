0b8a9f03c715017266791d1d5555f49d9d132acc
flapy.list@gmail.com <Lollipopjesus228>
2021-07-25 23:52:24 +0300

    signing in is added

--- app/src/main/java/com/example/fitnessfactory/MainActivity.java
+++ app/src/main/java/com/example/fitnessfactory/MainActivity.java
@@ -2,13 +2,75 @@ package com.example.fitnessfactory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnSignIn)
+    SignInButton btnSignIn;
+    @BindView(R.id.tvClientName)
+    TextView tvClientName;
+
+    Unbinder unbinder;
+    private final int RC_SIGN_IN = 1;
+    GoogleSignInClient signInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+        GoogleSignInOptions signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .build();
+        signInClient = GoogleSignIn.getClient(this, signInOptions);
+        btnSignIn.setOnClickListener(view -> {googleSignIn();});
+    }
+
+    private void googleSignIn() {
+        if (signInClient == null) {
+            return;
+        }
+        Intent signInIntent = signInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        tvClientName.setText(account.getDisplayName());
     }
 }
