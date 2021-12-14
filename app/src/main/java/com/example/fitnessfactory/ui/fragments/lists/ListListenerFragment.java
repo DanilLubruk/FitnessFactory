0216929f3790230b81f3d5a0ce170ebd9388d743
flapy.list@gmail.com <Lollipopjesus228>
2021-12-14 20:16:44 +0300

    Revert "list slitener refactoring"
    This reverts commit 83983e48

--- app/src/main/java/com/example/fitnessfactory/ui/fragments/lists/ListListenerFragment.java
+++ app/src/main/java/com/example/fitnessfactory/ui/fragments/lists/ListListenerFragment.java
@@ -15,8 +15,6 @@ import com.example.fitnessfactory.data.observers.SingleData;
 import com.example.fitnessfactory.databinding.FragmentListBinding;
 import com.example.fitnessfactory.ui.adapters.ListAdapter;
 import com.example.fitnessfactory.ui.fragments.BaseFragment;
-import com.example.fitnessfactory.ui.fragments.lists.helpers.ListListenerFragmentHelper;
-import com.example.fitnessfactory.ui.fragments.lists.listListeners.ListListener;
 import com.example.fitnessfactory.ui.viewholders.BaseRecyclerViewHolder;
 import com.example.fitnessfactory.ui.viewmodels.DataListListener;
 import com.example.fitnessfactory.ui.viewmodels.lists.ListViewModel;
@@ -36,10 +34,10 @@ public abstract class
 ListListenerFragment<
         ItemType,
         ViewHolderType extends BaseRecyclerViewHolder<ItemType>,
-        AdapterType extends ListAdapter<ItemType, ViewHolderType>> extends BaseFragment
-        implements ListListener<ItemType, ViewHolderType, AdapterType> {
+        AdapterType extends ListAdapter<ItemType, ViewHolderType>> extends BaseFragment {
 
-    protected ListListenerFragmentHelper<ItemType, ViewHolderType, AdapterType> helper;
+    protected AdapterType adapter;
+    protected RecyclerTouchListener touchListener;
 
     private FragmentListBinding binding;
 
@@ -47,13 +45,6 @@ ListListenerFragment<
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        helper = new ListListenerFragmentHelper<>(
-                getBaseActivity(),
-                binding.fabAddItem,
-                binding.rvData,
-                binding.pkProgress,
-                binding.tvEmptyData,
-                this);
         getBaseActivity().setTitle(getTitle());
         showProgress();
         defineViewModel();
@@ -71,24 +62,110 @@ ListListenerFragment<
         binding = FragmentListBinding.inflate(inflater, container, false);
     }
 
+    @Override
+    protected View getRootView() {
+        return binding.getRoot();
+    }
+
+    protected RecyclerView getRecyclerView() {
+        return binding.rvData;
+    }
+
+    protected FloatingActionMenu getFAB() {
+        return binding.fabAddItem;
+    }
+
+    protected ProgressBar getProgressBar() {
+        return binding.pkProgress;
+    }
+
+    protected TextView getEmptyDataLabel() {
+        return binding.tvEmptyData;
+    }
+
     protected void initComponents() {
-        helper.initComponents();
+        getFAB().setOnMenuButtonClickListener(view -> showEditorActivity(getNewItem()));
+        GuiUtils.initListView(getBaseActivity(), getRecyclerView(), false);
+        GuiUtils.setListViewAnimation(getRecyclerView(), getFAB());
+        touchListener = new RecyclerTouchListener(getBaseActivity(), getRecyclerView());
+        getRecyclerView().addOnItemTouchListener(touchListener);
+        touchListener.setSwipeOptionViews(R.id.btnEdit, R.id.btnDelete);
+        touchListener.setSwipeable(R.id.rowFG, R.id.rowBG, (viewId, position) -> {
+            switch (viewId) {
+                case R.id.btnEdit:
+                    showEditorActivity(adapter.getItem(position));
+                    break;
+                case R.id.btnDelete:
+                    askForDelete(adapter.getItem(position));
+                    break;
+            }
+        });
+        touchListener.setClickable(new RecyclerTouchListener.OnRowClickListener() {
+            @Override
+            public void onRowClicked(int position) {
+                onListRowClicked(adapter.getItem(position));
+            }
+
+            @Override
+            public void onIndependentViewClicked(int independentViewID, int position) {
+
+            }
+        });
+    }
+
+    protected void setListData(List<ItemType> listData) {
+        if (adapter == null) {
+            adapter = createNewAdapter(listData);
+            getRecyclerView().setAdapter(adapter);
+        } else {
+            adapter.setListData(listData);
+        }
+        closeProgress();
     }
 
     protected abstract String getTitle();
 
     protected abstract void defineViewModel();
 
-    @Override
-    public void deleteItem(ItemType item) {
+    protected abstract AdapterType createNewAdapter(List<ItemType> listData);
+
+    protected abstract void onListRowClicked(ItemType itemType);
+
+    protected abstract void showEditorActivity(ItemType item);
+
+    protected abstract ItemType getNewItem();
+
+    protected void askForDelete(ItemType item) {
+        subscribeInMainThread(
+                DialogUtils.showAskDialog(
+                        getBaseActivity(),
+                        getDeleteMessage(),
+                        ResUtils.getString(R.string.caption_ok),
+                        ResUtils.getString(R.string.caption_cancel)),
+                new SingleData<>(
+                        doDelete -> {
+                            if (doDelete) {
+                                deleteItem(item);
+                            }
+                        },
+                        throwable -> {
+                            throwable.printStackTrace();
+                            GuiUtils.showMessage(throwable.getLocalizedMessage());
+                        }
+                ));
+    }
+
+    protected void deleteItem(ItemType item) {
         getViewModel().deleteItem(item);
     }
+
+    protected abstract String getDeleteMessage();
 
     @Override
     public void onStart() {
         super.onStart();
         EventBus.getDefault().register(this);
-        if (getViewModel() != null) {
+        if (getViewModel() != null || doStartListenerInitially()) {
             getViewModel().startDataListener();
         }
     }
@@ -100,5 +177,25 @@ ListListenerFragment<
         if (getViewModel() != null) {
             getViewModel().stopDataListener();
         }
+    }
+
+    protected boolean doStartListenerInitially() {
+        return true;
+    }
+
+    public void closeProgress() {
+        getProgressBar().setVisibility(View.GONE);
+        getRecyclerView().setVisibility(View.VISIBLE);
+        getFAB().setVisibility(View.VISIBLE);
+        if (adapter != null) {
+            getEmptyDataLabel().setVisibility(adapter.getItemCount() == 0 ? View.VISIBLE : View.GONE);
+
+        }
+    }
+
+    public void showProgress() {
+        getProgressBar().setVisibility(View.VISIBLE);
+        getRecyclerView().setVisibility(View.GONE);
+        getFAB().setVisibility(View.GONE);
     }
 }
